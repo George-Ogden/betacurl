c36be19c186b52ca88daa42de3ce3fcdca42062c
George Ogden <george@goprogram.ai>
2023-01-31 14:36:52 +0000

    addded tests for log prob method

--- tests/test_gaussian.py
+++ tests/test_gaussian.py
@@ -4,9 +4,12 @@ from dm_env.specs import BoundedArray
 import tensorflow as tf
 import numpy as np
 
+single_action_spec = BoundedArray(minimum=(-100,), maximum=(100,), shape=(1,), dtype=np.float32)
+single_observation_spec = BoundedArray(minimum=(-100,), maximum=(100,), shape=(1,), dtype=np.float32)
 wide_action_spec = BoundedArray(minimum=(-100,-100), maximum=(100, 100), shape=(2,), dtype=np.float32)
 wide_observation_spec = BoundedArray(minimum=(-100,-100), maximum=(100, 100), shape=(2,), dtype=np.float32)
 wide_range_strategy = GaussianSamplingStrategy(action_spec=wide_action_spec, observation_spec=wide_observation_spec, latent_size=4)
+single_action_strategy = GaussianSamplingStrategy(action_spec=single_action_spec, observation_spec=single_observation_spec, latent_size=4)
 
 def test_samples_are_normal():
     means = np.ones(1000)
@@ -18,3 +21,9 @@ def test_samples_are_normal():
         std = float(tf.math.reduce_std(samples).numpy())
     assert .8 < mean < 1.2
     assert 1.5 < std < 2.5
+
+def test_log_probs_are_reasonable():
+    assert GaussianSamplingStrategy.compute_log_probs(((0.,),), ((0.,),), ((0.,),)) > GaussianSamplingStrategy.compute_log_probs(((0.,),), ((0.,),), ((1.,),))
+    assert GaussianSamplingStrategy.compute_log_probs(((0.,),), ((0.,),), ((0.,),)) > GaussianSamplingStrategy.compute_log_probs(((0.,),), ((1.,),), ((0.,),))
+    assert GaussianSamplingStrategy.compute_log_probs(((0.,),), ((0.,),), ((0.,),)) > GaussianSamplingStrategy.compute_log_probs(((0.,),), ((0.,),), ((1.,),))
+    assert np.allclose(GaussianSamplingStrategy.compute_log_probs(((1.,),), ((0.,),), ((0.5,),)), GaussianSamplingStrategy.compute_log_probs(((0.,),), ((0.,),), ((0.5,),)))
