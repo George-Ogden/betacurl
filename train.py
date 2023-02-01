6e0f118396d320f2c6f9dc94857fd6737560b4b3
George Ogden <george@goprogram.ai>
2023-02-01 09:47:22 +0000

    updated parsing

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ import wandb
 
 from simple_parsing.docstring import get_attribute_docstring
 from typing import get_type_hints
-from dataclasses import asdict
+from dataclasses import asdict, is_dataclass
 import argparse
 
 def main(args):
@@ -30,10 +30,10 @@ def main(args):
 # https://stackoverflow.com/a/66239222/12103577
 def get_dataclass_attributes_doc(some_dataclass):
     def get_attribute_unified_doc(some_dataclass, key):
-        """ returns a string that chains the above-comment, inline-comment and docstring """
+        """returns a string that chains the above-comment, inline-comment and docstring"""
         all_docstrings = get_attribute_docstring(some_dataclass, key)
         doc_list = asdict(all_docstrings).values()
-        return '\n'.join(doc_list)
+        return '\n'.join(doc_list).strip()
 
     attribute_docs = {}
     for key in get_type_hints(some_dataclass).keys():
@@ -42,22 +42,27 @@ def get_dataclass_attributes_doc(some_dataclass):
 
 def create_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    attribute_docs = get_dataclass_attributes_doc(CoachConfig) | get_dataclass_attributes_doc(SamplingEvaluatingPlayerConfig) | {"project_name": "wandb project name"}
+    attribute_docs = {
+        "project_name": "wandb project name"
+    }
     def add_argument(key, value):
         additional_options = {}
         if type(value) == bool:
             additional_options["action"] = "store_true"
         else:
             additional_options["type"] = type(value)
-        parser.add_argument(f"--{key}", default=value, help=attribute_docs[key].strip(), **additional_options)
-    for key, value in asdict(CoachConfig()).items():
-        if key in "model_filenames":
-            continue
-        if key == "player_config":
-            for sub_key, sub_value in value.items():
-                add_argument(sub_key, sub_value)
-        else:
-            add_argument(key, value)
+        parser.add_argument(f"--{key}", default=value, help=attribute_docs[key], **additional_options)
+
+    def add_dataclass(config):
+        nonlocal attribute_docs
+        attribute_docs |= get_dataclass_attributes_doc(type(config))
+        for k, v in asdict(config).items():
+            if is_dataclass(v):
+                add_dataclass(v)
+            else:
+                if len(attribute_docs.get(k, "")) > 0:
+                    add_argument(k, v)
+    add_dataclass(CoachConfig())
     add_argument("project_name", "test project")
     return parser
 
