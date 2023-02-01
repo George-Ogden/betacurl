269139585415fd0fa0e47da40a5bffaca997e082
George Ogden <george@goprogram.ai>
2023-02-01 13:58:52 +0000

    begun test refactor

---
+++ tests/config.py
@@ -0,0 +1,24 @@
+from pytest import fixture, mark
+from glob import glob
+import os
+
+SAVE_DIR = "test_save_dir"
+
+requires_cleanup = mark.usefixtures("cleanup")
+slow = mark.skip(reason="taking too long")
+display= mark.skipif("NO_DISPLAY" in os.environ, reason="no display")
+
+@fixture
+def cleanup():
+    yield
+    cleanup_dir(SAVE_DIR)
+
+def cleanup_dir(dir: str):
+    for filename in glob(f"{dir}/*"):
+        if os.path.isdir(filename):
+            cleanup_dir(filename)
+        else:
+            os.remove(filename)
+
+    if os.path.exists(dir):
+        os.rmdir(dir)
