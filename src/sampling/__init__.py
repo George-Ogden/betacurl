d98e773e74bed74ef3f6cc32a6c96ea85ca09cf3
George Ogden <george@goprogram.ai>
2023-02-08 19:48:08 +0000

    separate sampling config

--- src/sampling/__init__.py
+++ src/sampling/__init__.py
@@ -1,3 +1,4 @@
 from .nn import GaussianSamplingStrategy, NNSamplingStrategy, WeightedNNSamplingStrategy
+from .config import SamplerConfig, NNSamplerConfig, GaussianNNSamplerConfig
 from .random import RandomSamplingStrategy
 from .base import SamplingStrategy
