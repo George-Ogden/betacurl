c08b3ad6ee9c73aecb4345f2efb397104ad73b35
George Ogden <george@goprogram.ai>
2023-02-09 20:55:40 +0000

    create dense model

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,7 +1,8 @@
 from .config import ModelConfig, MLPModelConfig, TrainingConfig
-from .mlp import MLPModelFactory
 from .decorator import Learnable, ModelDecorator
 from .fcnn import MultiLayerModelFactory
+from .dense import DenseModelFactory
+from .mlp import MLPModelFactory
 from .base import ModelFactory
 
 BEST_MODEL_FACTORY = MultiLayerModelFactory()
---
+++ src/model/dense.py
@@ -0,0 +1,18 @@
+from tensorflow.keras import layers, Model
+from tensorflow import keras
+
+from typing import Optional
+
+from .config import ModelConfig
+from .base import ModelFactory
+
+class DenseModelFactory(ModelFactory):
+    CONFIG_CLASS = ModelConfig
+    @classmethod
+    def create_model(cls, input_size: int, output_size: int, config: Optional[ModelConfig] = ModelConfig()) -> Model:
+        return keras.Sequential(name="dense",
+            layers=[
+                keras.Input(shape=(input_size,)),
+                layers.Dense(output_size, activation=config.output_activation)
+            ]
+        )
