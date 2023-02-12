7fceb29d8cad7591497703dc8fe9c7f396cb8e36
George Ogden <george@goprogram.ai>
2023-02-12 19:52:47 +0000

    generate actions properly

--- src/sampling/nn/shared_torso.py
+++ src/sampling/nn/shared_torso.py
@@ -6,16 +6,16 @@ from typing import Callable, List, Optional, Tuple
 from dm_env.specs import BoundedArray
 
 from ...evaluation import NNEvaluationStrategy
-from ...model import DenseModelFactory, ModelDecorator, ModelFactory, TrainingConfig, BEST_MODEL_FACTORY
+from ...model import DenseModelFactory, ModelFactory, TrainingConfig, BEST_MODEL_FACTORY
 
 from ..config import SharedTorsoSamplerConfig
 from .gaussian import GaussianSamplingStrategy
 
-class SharedTorsoSamplingStrategy(NNEvaluationStrategy, GaussianSamplingStrategy, ModelDecorator):
+class SharedTorsoSamplingStrategy(GaussianSamplingStrategy, NNEvaluationStrategy):
     CONFIG_CLASS = SharedTorsoSamplerConfig
     DEFAULT_MODEL_FILE = "model.h5"
     def __init__(self, action_spec: BoundedArray, observation_spec: BoundedArray, model_factory: ModelFactory = BEST_MODEL_FACTORY, config: SharedTorsoSamplerConfig = SharedTorsoSamplerConfig()):
-        super(GaussianSamplingStrategy, self).__init__(action_spec=action_spec, observation_spec=observation_spec, config=config, model_factory=model_factory)
+        super().__init__(action_spec=action_spec, observation_spec=observation_spec, config=config, model_factory=model_factory)
 
     def setup_model(self, action_spec, observation_spec, model_factory, latent_size=0) -> tf.keras.Model:
         config = BEST_MODEL_FACTORY.CONFIG_CLASS(output_activation="linear")
@@ -27,35 +27,9 @@ class SharedTorsoSamplingStrategy(NNEvaluationStrategy, GaussianSamplingStrategy
         self.model = tf.keras.Model(inputs=feature_extractor.inputs, outputs=[policy_head, value_head])
         return self.model
 
-    def postprocess_actions(self, actions: tf.Tensor) -> tf.Tensor:
-        actions = tf.reshape(actions, shape=(-1, *self.action_shape))
-        actions *= self.action_range[1] - self.action_range[0]
-        actions += self.action_range[0]
-        return actions
-
-    def normalise_outputs(self, actions: np.ndarray) -> np.ndarray:
-        actions -= self.action_range[0]
-        actions /= self.action_range[1] - self.action_range[0]
-        return actions
-
-    def generate_actions(self, observation: np.ndarray, n: Optional[int] = None) -> np.ndarray:
-        batched_throughput = True
-        if n is None:
-            batched_throughput = False
-            n = 1
-
-        batch = np.tile(observation, (n, 1))
-        batch = self.preprocess_observations(batch)
-        input = self.add_noise_to_observations(batch)
-
-        samples = self.model.predict(input, batch_size=256, verbose=0)
-        samples = self.postprocess_actions(samples)
-
-        if not batched_throughput:
-            samples = tf.squeeze(samples, 0)
-
-        samples = samples.numpy()
-        return samples
+    def postprocess_actions(self, samples: tf.Tensor) -> tf.Tensor:
+        actions, values = samples
+        return super().postprocess_actions(actions)
 
     def learn(
         self,
