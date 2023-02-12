4a39b46cef6f70ce387cf28f95dc8b72ca76df06
George Ogden <george@goprogram.ai>
2023-02-12 19:37:25 +0000

    new naming convention

--- src/model/base.py
+++ src/model/base.py
@@ -6,6 +6,14 @@ from typing import Optional
 from .config import ModelConfig
 
 class ModelFactory(metaclass=ABCMeta):
+    NAME: str = ""
+    _model_count: int = 0
     @abstractclassmethod
     def create_model(cls, input_size: int, output_size: int, config: Optional[ModelConfig] = ModelConfig()) -> Model:
         ...
+
+    @classmethod
+    def get_name(cls):
+        name = f"{cls._model_count}_{cls.NAME}"
+        cls._model_count += 1
+        return name
--- src/model/dense.py
+++ src/model/dense.py
@@ -7,10 +7,11 @@ from .config import ModelConfig
 from .base import ModelFactory
 
 class DenseModelFactory(ModelFactory):
+    NAME = "dense"
     CONFIG_CLASS = ModelConfig
     @classmethod
     def create_model(cls, input_size: int, output_size: int, config: Optional[ModelConfig] = ModelConfig()) -> Model:
-        return keras.Sequential(name="dense",
+        return keras.Sequential(name=cls.get_name(),
             layers=[
                 keras.Input(shape=(input_size,)),
                 layers.Dense(output_size, activation=config.output_activation)
