cb38b37c5c66e13e7ee9abd75431d90eeb956be2
George Ogden <george@goprogram.ai>
2023-02-13 08:14:37 +0000

    create player for shared model

---
+++ src/game/player/shared_sampling_evaluating.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+
+from typing import Any, Callable, Iterable, List, Optional, Tuple
+from dm_env.specs import BoundedArray
+from numpy.typing import ArrayLike
+
+from copy import copy
+import numpy as np
+
+from ...sampling import SharedTorsoSamplingStrategy
+from ...model import TrainingConfig
+
+from ..game import GameSpec
+
+from .sampling_evaluating import SamplingEvaluatingPlayer
+from .config import SamplingEvaluatingPlayerConfig
+from .base import Player
+
+class SharedTorsoSamplingEvaluatingPlayer(SamplingEvaluatingPlayer):
+    SEPARATE_ATTRIBUTES = ["sampler_evaluator"]
+    def __init__(
+        self,
+        game_spec: GameSpec,
+        config: Optional[SamplingEvaluatingPlayerConfig]=SamplingEvaluatingPlayerConfig()
+    ):
+        super().__init__(
+            game_spec=game_spec,
+            SamplingStrategyClass=None,
+            EvaluationStrategyClass=None,
+            config=config
+            )
+    
+    def setup_sampler_evaluator(
+        self,
+        game_spec: GameSpec,
+        SamplingStrategyClass,
+        EvaluationStrategyClass,
+        config=SamplingEvaluatingPlayerConfig
+    ):
+        self.sampler_evaluator = SharedTorsoSamplingStrategy(
+            action_spec=game_spec.move_spec,
+            observation_spec=game_spec.observation_spec,
+            config=config
+        )
+
+    def learn(
+        self,
+        training_history: List[Tuple[int, np.ndarray, np.ndarray, float]],
+        augmentation_function: Callable[[np.ndarray, np.ndarray, float], List[Tuple[np.ndarray, np.ndarray, float]]],
+        training_config: TrainingConfig = TrainingConfig()
+    ):
+        self.sampler_evaluator.learn(training_history, augmentation_function, training_config)
