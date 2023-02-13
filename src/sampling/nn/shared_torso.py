af5fed11f93c55c16a47e31ef030ccaf877c583d
George Ogden <george@goprogram.ai>
2023-02-13 07:57:45 +0000

    added fit function

--- src/sampling/nn/shared_torso.py
+++ src/sampling/nn/shared_torso.py
@@ -1,4 +1,4 @@
-from tensorflow.keras import callbacks, layers
+from tensorflow.keras import callbacks, layers, losses
 import tensorflow as tf
 import numpy as np
 
@@ -16,6 +16,8 @@ class SharedTorsoSamplingStrategy(GaussianSamplingStrategy, NNEvaluationStrategy
     DEFAULT_MODEL_FILE = "model.h5"
     def __init__(self, action_spec: BoundedArray, observation_spec: BoundedArray, model_factory: ModelFactory = BEST_MODEL_FACTORY, config: SharedTorsoSamplerConfig = SharedTorsoSamplerConfig()):
         super().__init__(action_spec=action_spec, observation_spec=observation_spec, config=config, model_factory=model_factory)
+        # value function coefficient for loss calculation
+        self.vf_coef = config.vf_coefficient
 
     def setup_model(self, action_spec, observation_spec, model_factory, latent_size=0) -> tf.keras.Model:
         config = BEST_MODEL_FACTORY.CONFIG_CLASS(output_activation="linear")
@@ -35,15 +37,31 @@ class SharedTorsoSamplingStrategy(GaussianSamplingStrategy, NNEvaluationStrategy
         actions, values = samples
         return super().postprocess_values(values)
 
-    def learn(
-        self,
-        training_history: List[Tuple[int, np.ndarray, np.ndarray, float]],
-        augmentation_function: Callable[[np.ndarray, np.ndarray, float], List[Tuple[np.ndarray, np.ndarray, float]]],
-        training_config: TrainingConfig = TrainingConfig()
-    ) -> callbacks.History:
-        training_data = [(augmented_observation, augmented_action) for (player, observation, action, reward) in training_history for (augmented_observation, augmented_action, augmented_reward) in (augmentation_function(observation, action, reward) if np.sign(player) == np.sign(reward) else [])]
-        observations, actions = zip(*training_data)
+    def compute_loss(self, observations: np.ndarray, actions: tf.Tensor, rewards: tf.Tensor) -> tf.Tensor:
+        predicted_policy, predicted_values = self.model(
+            self.preprocess_observations(
+                observations
+            )
+        )
+        
+        target_policy, target_values = self.target_model(
+            self.preprocess_observations(
+                observations
+            )
+        )
+        
+        predicted_distribution = self.generate_distribution(predicted_policy)
+        target_distribution = self.generate_distribution(target_policy)
+        advantages = rewards - tf.squeeze(target_values, axis=-1)
 
-        observations = self.add_noise_to_observations(observations)
+        policy_loss = self.ppo_clip_loss(
+            predicted_distribution=predicted_distribution,
+            target_distribution=target_distribution,
+            actions=actions,
+            advantages=advantages
+        )
+        
+        value_loss = losses.mean_squared_error(rewards, tf.squeeze(predicted_values, axis=-1))
 
-        return self.fit(observations, actions, training_config)
+        return value_loss * 0+  policy_loss
+        return policy_loss + self.vf_coef * value_loss
