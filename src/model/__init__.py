a21d0fab4ed11f7816fb4a55d2e651439322b275
George Ogden <george@goprogram.ai>
2023-02-20 11:28:55 +0000

    remove leftover LSTM code

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -2,7 +2,6 @@ from .config import ModelConfig, MLPModelConfig, TrainingConfig
 from .decorator import Learnable, ModelDecorator
 from .fcnn import MultiLayerModelFactory
 from .dense import DenseModelFactory
-from .lstm import LSTMModelFactory
 from .mlp import MLPModelFactory
 from .base import ModelFactory
 
--- src/sampling/nn/__init__.py
+++ src/sampling/nn/__init__.py
@@ -1,5 +1,4 @@
 from .shared_torso import SharedTorsoSamplingEvaluatingStrategy
 from .weighted import WeightedNNSamplingStrategy
 from .gaussian import GaussianSamplingStrategy
-from .lstm import LSTMSamplingStrategy
 from .nn import NNSamplingStrategy
