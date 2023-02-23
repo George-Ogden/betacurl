79844fb85c0df9c947bb075fccc2f5c6b6e34e88
George Ogden <george@goprogram.ai>
2023-02-23 11:02:45 +0000

    test multiple model io

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,12 +1,10 @@
-from src.curling import SingleEndCurlingGame
-from src.io import SaveableObject
+import numpy as np
+
+from src.model import ModelConfig, DenseModelFactory, BEST_MODEL_FACTORY
+from src.io import SaveableMultiModel, SaveableObject
 
 from tests.config import cleanup, requires_cleanup, SAVE_DIR
 from tests.utils import generic_save_test
-
-game = SingleEndCurlingGame()
-observation_spec = game.game_spec.observation_spec
-move_spec = game.game_spec.move_spec
 
 def save_load(object: SaveableObject) -> SaveableObject:
     object.save(SAVE_DIR)
@@ -32,3 +30,30 @@ def test_saveable_object_saves_and_loads():
 
     # cleanup
     SaveableObject.DEFAULT_FILENAME = None
+
+@requires_cleanup
+def test_saveable_multimodel_saves_and_loads():
+    orig_model = SaveableMultiModel()
+    orig_model.m1 = BEST_MODEL_FACTORY.create_model(20, 30)
+    orig_model.m2 = DenseModelFactory.create_model(
+        4,
+        8,
+        ModelConfig(
+            output_activation="tanh"
+        )
+    )
+    orig_model.DEFAULT_FILENAME = "file"
+    orig_model.MODELS = {"m1": "m1.h5", "m2": "m2.h5"}
+    orig_model.m1(np.random.randn(1, 20))
+    orig_model.m2(np.random.randn(1, 4))
+
+    loaded_model = save_load(orig_model)
+    assert loaded_model.DEFAULT_FILENAME == orig_model.DEFAULT_FILENAME
+    assert list(loaded_model.MODELS.keys()) == list(orig_model.MODELS.keys())
+    assert list(loaded_model.MODELS.values()) == list(orig_model.MODELS.values())
+
+    x = np.random.randn(8, 20)
+    assert np.allclose(orig_model.m1(x), loaded_model.m1(x))
+
+    x = np.random.randn(8, 4)
+    assert np.allclose(orig_model.m2(x), loaded_model.m2(x))
