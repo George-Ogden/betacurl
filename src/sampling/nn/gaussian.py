28c5acd58603690bddf68d72768ecc1def221c6f
George Ogden <george@goprogram.ai>
2023-02-26 18:41:41 +0000

    abstract custom decorator

--- src/sampling/nn/gaussian.py
+++ src/sampling/nn/gaussian.py
@@ -1,19 +1,18 @@
 from tensorflow.keras import callbacks, models, utils
 from tensorflow_probability import distributions
-from copy import copy
 from tensorflow import data
 import tensorflow as tf
+from copy import copy
 import numpy as np
-
 
 from typing import Callable, List, Tuple
 from dm_env.specs import BoundedArray
 
-from ...model import ModelFactory, TrainingConfig, BEST_MODEL_FACTORY
+from ...model import CustomDecorator, ModelFactory, TrainingConfig, BEST_MODEL_FACTORY
 from ..config import GaussianSamplerConfig
 from .nn import NNSamplingStrategy
 
-class GaussianSamplingStrategy(NNSamplingStrategy):
+class GaussianSamplingStrategy(CustomDecorator, NNSamplingStrategy):
     CONFIG_CLASS = GaussianSamplerConfig
     def __init__(self, action_spec: BoundedArray, observation_spec: BoundedArray, model_factory: ModelFactory = BEST_MODEL_FACTORY, config: GaussianSamplerConfig = GaussianSamplerConfig()):
         super().__init__(action_spec, observation_spec, model_factory, config)
@@ -50,7 +49,13 @@ class GaussianSamplingStrategy(NNSamplingStrategy):
         log_probs = distribution.log_prob(actions)
         return tf.reduce_sum(log_probs, axis=-1)
 
-    def compute_loss(self, observations: np.ndarray, actions: tf.Tensor, values: tf.Tensor, advantage: tf.Tensor, target_log_probs: tf.Tensor) -> tf.Tensor:
+    def compute_loss(self,
+        observations: np.ndarray,
+        actions: tf.Tensor,
+        values: tf.Tensor,
+        advantage: tf.Tensor,
+        target_log_probs: tf.Tensor
+    ) -> tf.Tensor:
         predicted_distribution = self.generate_distribution(
             self.model(
                 self.preprocess_observations(
@@ -78,50 +83,6 @@ class GaussianSamplingStrategy(NNSamplingStrategy):
         loss = tf.math.minimum(loss_1, loss_2)
 
         return -tf.reduce_mean(loss)
-
-    def train_step(self, batch: np.ndarray, optimizer: tf.optimizers.Optimizer) -> np.ndarray:
-        with tf.GradientTape() as tape:
-            loss = self.compute_loss(*batch)
-        grads = tape.gradient(loss, self.model.trainable_variables)
-        optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
-        return loss
-
-    def fit(self, dataset: data.Dataset, training_config: TrainingConfig = TrainingConfig()) -> callbacks.History:
-        training_config = copy(training_config)
-        train_dataset, val_dataset = utils.split_dataset(dataset, right_size=training_config.validation_split, shuffle=True)
-        optimizer = training_config.optimizer
-        optimizer.clipnorm = self.max_grad_norm
-        batch_size = training_config.batch_size
-        training_config.metrics = []
-
-        history = callbacks.History()
-        callback = callbacks.CallbackList(
-            training_config.callbacks + [history],
-            model=self.model,
-            add_history=False,
-            add_progbar=training_config.verbose != 0,
-            verbose=training_config.verbose,
-            epochs=training_config.training_epochs,
-            steps=(len(train_dataset) - 1) // training_config.batch_size + 1,
-        )
-
-        self.model.stop_training = False
-        callback.on_train_begin()
-        for epoch in range(training_config.training_epochs):
-            callback.on_epoch_begin(epoch)
-            loss = 0
-            for step, batch in enumerate(train_dataset.batch(batch_size)):
-                callback.on_train_batch_begin(step)
-                loss += self.train_step(batch, optimizer)
-                callback.on_train_batch_end(step)
-            val_loss = 0
-            for step, batch in enumerate(val_dataset.batch(batch_size)):
-                val_loss += self.compute_loss(*batch)
-            callback.on_epoch_end(epoch, {"loss": loss / len(train_dataset), "val_loss": val_loss / len(val_dataset)})
-            if self.model.stop_training:
-                break
-        callback.on_train_end()
-        return history
 
     def compute_advantages_and_target_log_probs(
         self,
@@ -168,6 +129,6 @@ class GaussianSamplingStrategy(NNSamplingStrategy):
         secondary_dataset = self.create_dataset(zip(*flattened_transform))
 
         training_config.optimizer_kwargs["clipnorm"] = self.max_grad_norm
-        self.compile_model(training_config)
+        training_config.metrics = []
         
         return self.fit(secondary_dataset, training_config)
