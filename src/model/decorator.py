608eb6bf8596cf9e42909a62088233487f081890
George Ogden <george@goprogram.ai>
2023-03-05 19:03:02 +0000

    decorator merge utility

--- src/model/decorator.py
+++ src/model/decorator.py
@@ -1,10 +1,10 @@
 from tensorflow.keras import callbacks, utils
+from copy import copy, deepcopy
 from tensorflow import data
 import tensorflow as tf
-from copy import copy
 import numpy as np
 
-from typing import Any, Dict, List, Tuple
+from typing import Any, Dict, List, Tuple, Union
 from abc import abstractmethod, ABCMeta
 
 from ..utils import SaveableModel
@@ -33,7 +33,35 @@ class ModelDecorator(SaveableModel, Learnable):
         self.model.compile(**compile_options)
 
     @staticmethod
-    def create_dataset(dataset: List[Tuple[float]]) -> data.Dataset:
+    def merge(*items: List[Any]):
+        assert len(items) > 0
+        shared_item = deepcopy(items[0])
+        using_dict = isinstance(shared_item, dict)
+        for item in items[1:]:
+            for k, w in (item if using_dict else vars(item)).items():
+                v = shared_item.get(k, None) if using_dict else getattr(shared_item, k, None)
+                if v is None:
+                    v = w
+                else:
+                    # "Irrefutable pattern is allowed only for the last case statement"
+                    # when matching `type(v)`
+                    match type(v).__name__:
+                        case "bool":
+                            v |= w
+                        case "list":
+                            v.extend(w)
+                        case "int":
+                            v += w
+                        case "dict":
+                            v = ModelDecorator.merge(v, w)
+                if using_dict:
+                    shared_item[k] = v
+                else:
+                    setattr(shared_item, k, v)
+        return shared_item
+
+    @staticmethod
+    def create_dataset(dataset: List[Tuple[Union[np.ndarray, float]]]) -> data.Dataset:
         transposed_data = tuple(np.array(data, dtype=np.float32) for data in zip(*dataset))
         return data.Dataset.from_tensor_slices(transposed_data)
 
@@ -50,17 +78,19 @@ class ModelDecorator(SaveableModel, Learnable):
         self,
         X: np.ndarray,
         Y: np.ndarray,
-        training_config: TrainingConfig
+        training_config: TrainingConfig,
+        convert_to_numpy:bool=True
     ) -> Tuple[np.ndarray, np.ndarray, Dict[str, Any]]:
         """prepare inputs for Model.fit style args
 
         Returns:
             Tuple[np.ndarray, np.ndarray, Dict[str, Any]]: X, Y, kwargs
         """
-        if type(X) != np.ndarray:
-            X = np.array(X, dtype=np.float32)
-        if type(Y) != np.ndarray:
-            Y = np.array(Y, dtype=np.float32)
+        if convert_to_numpy:
+            if type(X) != np.ndarray:
+                X = np.array(X, dtype=np.float32)
+            if type(Y) != np.ndarray:
+                Y = np.array(Y, dtype=np.float32)
 
         self.compile_model(training_config)
 
@@ -86,15 +116,16 @@ class CustomDecorator(ModelDecorator):
         with tf.GradientTape() as tape:
             loss = self.compute_loss(*batch)
         grads = tape.gradient(loss, self.model.trainable_variables)
-        optimizer.apply_gradients(
-            zip(
-                [
-                    tf.where(tf.math.is_nan(grad), tf.zeros_like(grad), grad)
-                    for grad in grads
-                ],
-                self.model.trainable_variables
-            )
-        )
+        optimizer.apply_gradients([
+            (
+                tf.where(tf.math.is_nan(grad),tf.zeros_like(grad), grad),
+                var
+            ) for grad, var in
+                zip(
+                    grads,
+                    self.model.trainable_variables
+                ) if grad is not None
+        ])
         return loss
 
     def fit(self, dataset: data.Dataset, training_config: TrainingConfig = TrainingConfig()) -> callbacks.History:
