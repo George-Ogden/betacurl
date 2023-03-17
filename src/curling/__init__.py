5ebd741126e59ac1fb480ee5150b1582a5e7d2f9
George Ogden <george@goprogram.ai>
2023-03-17 13:43:42 +0000

    move stone to separate file

--- src/curling/__init__.py
+++ src/curling/__init__.py
@@ -1,6 +1,7 @@
 from .constants import PhysicalConstants, SimulationConstants
-from .curling import Curling, Stone, StoneThrow
 from .game import SingleEndCurlingGame
+from .stone import Stone, StoneThrow
 from .enums import StoneColor
+from .curling import Curling
 
 CURLING_GAME = SingleEndCurlingGame()
