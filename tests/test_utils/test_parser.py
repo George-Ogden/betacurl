2f8d3d4e7d6c0b1b7f1e1568203af33cf0287441
George Ogden <george@goprogram.ai>
2023-03-19 17:43:05 +0000

    test args to dataclass

--- tests/test_utils/test_parser.py
+++ tests/test_utils/test_parser.py
@@ -9,7 +9,8 @@ class SimpleConfig(Config):
     a: str = "a"
     """A"""
     b: int = 2
-    c: ClassVar[float] = 3.
+    """B"""
+    c: float = 3.
 
 @dataclass
 class ComplexConfig(Config):
@@ -24,7 +25,7 @@ class ComplexConfig(Config):
 def test_doc():
     doc = ParserBuilder.get_dataclass_attributes_doc(SimpleConfig)
     assert doc["a"] == "A"
-    assert not "b" in doc
+    assert doc["b"] == "B"
     assert not "c" in doc
 
     doc = ParserBuilder.get_dataclass_attributes_doc(ComplexConfig)
@@ -35,19 +36,20 @@ def test_doc():
 def test_create_parser():
     parser = ParserBuilder().add_dataclass(ComplexConfig()).build()
     args = parser.parse_args([])
+
     assert args.a == "a"
+    assert args.b == 2
     assert args.x == "x"
     assert args.y == 2
     assert args.z == 3.
-    for arg in ("b","c"):
-        assert not arg in args
+    assert not "c" in args
 
 def test_create_parser_with_additional_args(capsys):
     parser = ParserBuilder().add_dataclass(
         SimpleConfig()
     ).add_argument(
-        key="project_name",
-        value="DEFAULT VALUE",
+        name="project_name",
+        default="DEFAULT VALUE",
         help="TEST PROJECT HELP",
     ).build()
     parser.print_help()
@@ -59,3 +61,47 @@ def test_create_parser_with_additional_args(capsys):
     args = parser.parse_args([])
     assert args.a == "a"
     assert args.project_name == "DEFAULT VALUE"
+
+def test_config_set_args():
+    simple_config = SimpleConfig(a="A")
+    args = ParserBuilder().add_dataclass(simple_config).build().parse_args(["--b", "8"])
+    simple_config.set_args(args)
+
+    assert simple_config.a == "A"
+    assert simple_config.b == 8
+    assert simple_config.c == 3.
+
+    complex_config = ComplexConfig(x="X", simple_config=simple_config)
+    args = ParserBuilder().add_dataclass(complex_config).build().parse_args(["--b", "6", "--z", ".9"])
+    complex_config.set_args(args)
+
+    assert complex_config.x == "X"
+    assert complex_config.y == 2
+    assert complex_config.z == .9
+
+    simple_config = complex_config.simple_config
+    assert simple_config.a == "A"
+    assert simple_config.b == 6
+    assert simple_config.c == 3.
+
+def test_config_from_args():
+    simple_config = SimpleConfig(a="A", c=4.)
+    args = ParserBuilder().add_dataclass(simple_config).build().parse_args(["--b", "8"])
+    simple_config = SimpleConfig.from_args(args)
+
+    assert simple_config.a == "A"
+    assert simple_config.b == 8
+    assert simple_config.c == 3.
+
+    complex_config = ComplexConfig(x="X", simple_config=simple_config)
+    args = ParserBuilder().add_dataclass(complex_config).build().parse_args(["--b", "6", "--z", ".9"])
+    complex_config = ComplexConfig.from_args(args)
+
+    assert complex_config.x == "X"
+    assert complex_config.y == 2
+    assert complex_config.z == .9
+
+    simple_config = complex_config.simple_config
+    assert simple_config.a == "A"
+    assert simple_config.b == 6
+    assert simple_config.c == 3.
