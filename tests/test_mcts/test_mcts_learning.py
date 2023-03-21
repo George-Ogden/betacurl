9c5643f994f0b5dc0c1abbd305cd89e5cb915b8a
George Ogden <george@goprogram.ai>
2023-03-21 12:02:58 +0000

    test

--- tests/test_mcts/test_mcts_learning.py
+++ tests/test_mcts/test_mcts_learning.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from pytest import mark
 
-from src.mcts import MCTSModel, MCTSModelConfig
+from src.mcts import MCTSModel, MCTSModelConfig, PPOMCTSModel, PPOMCTSModelConfig
 from src.model import TrainingConfig
 
 from tests.utils import MDPStubGame, StubGame
@@ -112,3 +112,85 @@ def test_model_learns_from_multiple_actions():
     assert tf.reduce_all(model.generate_distribution(game.get_observation()).loc > .5)
     game.reset(0)
     assert tf.reduce_all(model.generate_distribution(game.get_observation()).loc > .5)
+
+@mark.probabilistic
+def test_ppo_model_losses_converge():
+    model = PPOMCTSModel(
+        game_spec,
+        config=PPOMCTSModelConfig(
+            vf_coeff=.5,
+            ent_coeff=0.,
+            target_kl=None
+        )
+    )
+
+    for _ in range(10):
+        model.learn(
+            training_data,
+            stub_game.no_symmetries,
+            training_config=TrainingConfig(
+                training_epochs=10,
+                lr=1e-2
+            )
+        )
+
+    distribution = model.generate_distribution(training_data[0][1])
+    assert np.abs(model.predict_values(training_data[0][1]) - result) < stub_game.max_move
+    assert tf.reduce_all(distribution.loc > .75 * game_spec.move_spec.maximum)
+
+@mark.probabilistic
+def test_kl_divergence_without_early_stopping():
+    model = PPOMCTSModel(
+        game_spec=game_spec,
+        config=PPOMCTSModelConfig(
+            target_kl=None,
+            vf_coeff=0.,
+            ent_coeff=0.
+        )
+    )
+    training_sample = training_data[0]
+    initial_distribution = model.generate_distribution(
+        training_sample[1]
+    )
+    training_data[-1] = [(initial_distribution.mean() + 100, 1.), (initial_distribution.mean() - 100, -1.)]
+    model.learn(
+        training_data=[training_sample] * 10,
+        augmentation_function=stub_game.no_symmetries,
+        training_config=TrainingConfig(
+            training_epochs=1000,
+            lr=1e-3,
+            batch_size=1
+        )
+    )
+    assert tf.reduce_mean(initial_distribution.kl_divergence(
+        model.generate_distribution(training_sample[1])
+    )) > 5
+    
+
+@mark.probabilistic
+def test_kl_divergence_with_early_stopping():
+    model = PPOMCTSModel(
+        game_spec=game_spec,
+        config=PPOMCTSModelConfig(
+            target_kl=2.,
+            vf_coeff=0.,
+            ent_coeff=0.
+        )
+    )
+    training_sample = training_data[0]
+    initial_distribution = model.generate_distribution(
+        training_sample[1]
+    )
+    training_data[-1] = [(initial_distribution.mean() + 100, 1.), (initial_distribution.mean() - 100, -1.)]
+    model.learn(
+        training_data=[training_sample] * 10,
+        augmentation_function=stub_game.no_symmetries,
+        training_config=TrainingConfig(
+            training_epochs=1000,
+            lr=1e-3,
+            batch_size=1
+        )
+    )
+    assert tf.reduce_mean(initial_distribution.kl_divergence(
+        model.generate_distribution(training_sample[1])
+    )) < 5
