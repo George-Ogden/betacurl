9df59ae9835c6ad7b9dd9552cf47ac0413ca6625
George Ogden <george@goprogram.ai>
2023-04-13 14:04:02 +0100

    added sampling and probs

--- src/mcts/model/fourier.py
+++ src/mcts/model/fourier.py
@@ -58,15 +58,49 @@ class FourierDistribution(distributions.Distribution):
             0
         )
         self.pdf /= tf.reduce_sum(self.pdf, axis=-1, keepdims=True)
-        self.cdf = tf.cumsum(self.pdf, axis=-1)
+        self.cdf = tf.concat((tf.cumsum(self.pdf, axis=-1), tf.maximum(1., tf.reduce_sum(self.pdf, axis=-1, keepdims=True))), axis=-1)
         self.points = tf.linspace(*range, self.granularity)
         self.range = range
 
-    def _mean(self):
+    def _sample_shape(self) -> Tuple[int, ...]:
+        return ()
+
+    def _batch_shape(self) -> Tuple[int, ...]:
+        return self.pdf.shape[:-1]
+
+    def _sample_n(self, n: int, seed=None) -> tf.Tensor:
+        # sample n points from the cdf using linear interpolation
+        random_points = tf.random.uniform(
+            (len(self.pdf), n),
+            dtype=self.dtype,
+            seed=seed
+        )
+        return tf.transpose(
+            tf.gather(
+                self.points,
+                tf.cast(
+                    tf.searchsorted(
+                        self.cdf,
+                        random_points,
+                        side="right"
+                    ),
+                    tf.int32
+                )
+            )
+        )
+
+    def _mean(self) -> tf.Tensor:
         return tf.reduce_sum(self.points * self.pdf, axis=-1)
 
-    def _variance(self):
+    def _variance(self) -> tf.Tensor:
         return tf.reduce_sum(self.points ** 2 * self.pdf, axis=-1) - self._mean() ** 2
 
-    def _mode(self):
+    def _mode(self) -> tf.Tensor:
         return tf.gather(self.points, tf.argmax(self.pdf, axis=-1))
+
+    def prob(self, action: tf.Tensor) -> tf.Tensor:
+        # interpolate between pdf values
+        scaled = (action - self.range[0]) / (self.range[1] - self.range[0]) * (self.granularity - 1)
+        indices = tf.minimum(tf.cast(scaled, tf.int32), self.granularity - 2)
+        delta = scaled - tf.cast(indices, self.dtype)
+        return tf.gather(self.pdf, indices, axis=-1) * delta + tf.gather(self.pdf, indices + 1, axis=-1) * (1 - delta)
