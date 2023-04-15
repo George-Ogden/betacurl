b8bff8dbf88ccecdb6569dd4961006549adbd4d9
George Ogden <george@goprogram.ai>
2023-04-15 10:38:13 +0100

    modify tests for multivariate distribution

--- tests/test_mcts/test_mcts_model/test_fourier_model.py
+++ tests/test_mcts/test_mcts_model/test_fourier_model.py
@@ -13,6 +13,10 @@ test_distribution = FourierDistribution(
     coefficients = tf.reshape(tf.range(24, dtype=tf.float32), (4, 3, 2)),
     range = tf.constant([[2., 4.] for _ in range(4)]),
 )
+test_multi_distribution = FourierDistribution(
+    coefficients = tf.reshape(tf.range(120, dtype=tf.float32), (5, 4, 3, 2)),
+    range = tf.constant([[[2., 4.] for _ in range(4)] for _ in range(5)]),
+)
 
 max_move = MDPStubGame.max_move
 action_size = MDPStubGame.action_size
@@ -40,16 +44,37 @@ def test_distribution_pdf_cdf():
     assert np.allclose(test_distribution.cdf[:, 0], 0., atol=1e-2)
     assert np.allclose(test_distribution.cdf[:,-1], 1., atol=1e-2)
 
+    assert tf.reduce_all(test_multi_distribution.pdf >= 0)
+    assert tf.reduce_all(0 <= test_multi_distribution.cdf) and tf.reduce_all(test_multi_distribution.cdf <= 1+1e-2)
+    assert np.allclose(test_multi_distribution.cdf[:, 0], 0., atol=1e-2)
+    assert np.allclose(test_multi_distribution.cdf[:,-1], 1., atol=1e-2)
+
 def test_distribution_stats():
     mean = test_distribution.mean()
     mode = test_distribution.mode()
     std = test_distribution.stddev()
     variance = test_distribution.variance()
 
-    assert mean.shape in {(4,), (4, 1)}
-    assert mode.shape in {(4,), (4, 1)}
-    assert std.shape in {(4,), (4, 1)}
-    assert variance.shape in {(4,), (4, 1)}
+    assert mean.shape == (4,)
+    assert mode.shape == (4,)
+    assert std.shape == (4,)
+    assert variance.shape == (4,)
+
+    assert tf.reduce_all(2 <= mean) and tf.reduce_all(mean <= 4)
+    assert tf.reduce_all(2 <= mode) and tf.reduce_all(mode <= 4)
+    assert tf.reduce_all(0 < variance) and tf.reduce_all(variance < 1)
+    assert np.allclose(std, tf.sqrt(variance))
+
+def test_multi_distribution_stats():
+    mean = test_multi_distribution.mean()
+    mode = test_multi_distribution.mode()
+    std = test_multi_distribution.stddev()
+    variance = test_multi_distribution.variance()
+
+    assert mean.shape == (5,4)
+    assert mode.shape == (5,4)
+    assert std.shape == (5,4)
+    assert variance.shape == (5,4)
 
     assert tf.reduce_all(2 <= mean) and tf.reduce_all(mean <= 4)
     assert tf.reduce_all(2 <= mode) and tf.reduce_all(mode <= 4)
@@ -57,10 +82,33 @@ def test_distribution_stats():
     assert np.allclose(std, tf.sqrt(variance))
 
 def test_distribution_sample():
-    for batch in test_distribution.sample(100):
-        assert batch.shape in {(4,1), (4,1)}
-        assert tf.reduce_all(2 <= batch) and tf.reduce_all(batch <= 4)
-        assert tf.reduce_all(test_distribution.prob(batch) >= 0)
+    samples = test_distribution.sample(100)
+    assert samples.shape == (100, 4)
+    assert tf.reduce_all(2 <= samples) and tf.reduce_all(samples <= 4)
+    probs = test_distribution.prob(samples)
+    assert probs.shape == (100, 4)
+    assert tf.reduce_all(probs >= 0)
+
+def test_multi_distribution_sample():
+    samples = test_multi_distribution.sample(100)
+    assert samples.shape == (100, 5, 4)
+    assert tf.reduce_all(2 <= samples) and tf.reduce_all(samples <= 4)
+    probs = test_multi_distribution.prob(samples)
+    assert probs.shape == (100, 5, 4)
+    assert tf.reduce_all(probs >= 0)
+
+@mark.flaky
+def test_distribution_correlations():
+    samples = test_distribution.sample(1000).numpy()
+    correlations = np.corrcoef(samples, rowvar=False) - np.eye(4)
+    assert np.allclose(correlations, 0., atol=.8)
+
+    samples = test_multi_distribution.sample(1000).numpy()
+    correlations = np.corrcoef(samples.reshape(1000, 20), rowvar=False) - np.eye(20)
+    assert np.allclose(correlations, 0., atol=.8)
+
+    correlations = np.corrcoef(samples.reshape(5000, 4), rowvar=False) - np.eye(4)
+    assert np.allclose(correlations, 0., atol=.8)
 
 def test_config_is_used():
     model = FourierMCTSModel(
