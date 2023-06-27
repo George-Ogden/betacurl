32b72216f862ba96bba18fc24cba2fa08f799ad4
George Ogden <g-ogden@instadeep.com>
2023-06-27 12:28:21 +0200

    parameterise IO tests

--- tests/test_mcts/test_mcts_io.py
+++ tests/test_mcts/test_mcts_io.py
@@ -3,11 +3,14 @@ from src.player import MCTSPlayer, NNMCTSPlayer
 from tests.utils import MDPStubGame, generic_save_load_test, generic_save_test
 from tests.config import cleanup, requires_cleanup
 
+from pytest import mark
+
 game = MDPStubGame(6)
 game.reset()
 
 @requires_cleanup
-def test_player_io_without_mcts():
+@mark.parametrize("Player", [MCTSPlayer, NNMCTSPlayer])
+def test_player_io_without_mcts(Player):
     player = MCTSPlayer(
         game.game_spec
     )
@@ -24,11 +27,3 @@ def test_player_io_with_mcts():
     original, copy = generic_save_load_test(player, excluded_attrs=["mcts"])
     assert hasattr(original, "mcts")
     assert hasattr(copy, "mcts")
-
-@requires_cleanup
-def test_nn_player_io_without_model():
-    player = NNMCTSPlayer(
-        game.game_spec
-    )
-    generic_save_test(player)
-    generic_save_load_test(player)
